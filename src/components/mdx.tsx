76eb737898b8274c8fd5b44e8dcc82203f901776
rifkiahmafahrezi <rifkiaf589@gmail.com>
2026-08-23 07:15:48 +0700

    refactor: consolidate registry demo exports and update MDX component mapping

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -3,17 +3,7 @@ import type { MDXComponents } from 'mdx/types';
 import { InstallationTabs } from './installation-tabs';
 import { PreviewComponent } from './preview-component';
 
-import { TextFieldDemo } from '../registry/demos/text-field-demo';
-import { TextFieldPlaygroundDemo } from '../registry/demos/text-field-playground-demo';
-import { TextFieldFormDemo } from '../registry/demos/text-field-form-demo';
-
-import { NumberFieldDemo } from '../registry/demos/number-field-demo';
-import { NumberFieldPlaygroundDemo } from '../registry/demos/number-field-playground';
-import { NumberFieldFormDemo } from '../registry/demos/number-field-form-demo';
-
-import { TextareaFieldDemo } from '../registry/demos/textarea-field-demo';
-import { TextareaFieldPlaygroundDemo } from '../registry/demos/textarea-field-playground-demo';
-import { TextareaFieldFormDemo } from '../registry/demos/textarea-field-form-demo';
+import * as demos from "@/registry/demos"
 
 export function getMDXComponents(components?: MDXComponents) {
   return {
@@ -22,15 +12,7 @@ export function getMDXComponents(components?: MDXComponents) {
     InstallationTabs,
     PreviewComponent,
     ComponentPreview: PreviewComponent,
-    TextFieldDemo,
-    TextFieldPlaygroundDemo,
-    NumberFieldDemo,
-    NumberFieldPlaygroundDemo,
-    NumberFieldFormDemo,
-    TextFieldFormDemo,
-    TextareaFieldDemo,
-    TextareaFieldPlaygroundDemo,
-    TextareaFieldFormDemo,
+    ...demos
   } satisfies MDXComponents;
 }
 
